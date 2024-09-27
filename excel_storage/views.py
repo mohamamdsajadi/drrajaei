6e30a505e31d5eed42dca45b85809d08eac66aca
mohammadsajadi <mohammadsajadi50@gmail.com>
2024-09-27 22:46:21 +0330

    add get hashed file address

--- excel_storage/views.py
+++ excel_storage/views.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 
 from django.core.files.storage.filesystem import FileSystemStorage
 from django.http import JsonResponse
@@ -12,8 +11,9 @@ from drRajaei import settings
 def add_excel_file(request):
     if request.method == 'POST':
         file = request.FILES['excel']
-        fs = FileSystemStorage(location=os.path.join(settings.MEDIA_ROOT, 'X'))
-        fs2 = FileSystemStorage(location=os.path.join(settings.MEDIA_ROOT, 'Y'))
+        fs = FileSystemStorage(location=os.path.join(settings.MEDIA_ROOT, 'grouped_files'))
+        fs2 = FileSystemStorage(location=os.path.join(settings.MEDIA_ROOT, 'XVGSAWE'))
         file_name = fs.save(file.name, file)
-        fs2.save(file.name, file)
-        return JsonResponse({'address': request.build_absolute_uri(fs.url(file_name)) })
+        fs2.save(hash(file.name), file)
+        return JsonResponse({'clear_file_address': request.build_absolute_uri(fs.url(file_name)),
+                             'hashed_address': request.build_absolute_uri(fs2.url(file_name))})
