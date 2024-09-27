712b1413ab5ee8064d8d1758a0300a79e8d9d7f8
mohammadsajadi <mohammadsajadi50@gmail.com>
2024-09-27 23:04:46 +0330

    refact

--- excel_storage/views.py
+++ excel_storage/views.py
@@ -11,9 +11,11 @@ from drRajaei import settings
 def add_excel_file(request):
     if request.method == 'POST':
         file = request.FILES['excel']
-        fs = FileSystemStorage(location=os.path.join(settings.MEDIA_ROOT, 'grouped_files'))
+        fs = FileSystemStorage(location=os.path.join(settings.MEDIA_ROOT,
+                                                     'grouped_files',
+                                                     file.name.split('.')[0]))
         fs2 = FileSystemStorage(location=os.path.join(settings.MEDIA_ROOT, 'XVGSAWE'))
         file_name = fs.save(file.name, file)
-        fs2.save(hash(file.name), file)
+        hashed_file = fs2.save(hash(file.name), file)
         return JsonResponse({'clear_file_address': request.build_absolute_uri(fs.url(file_name)),
-                             'hashed_address': request.build_absolute_uri(fs2.url(file_name))})
+                             'hashed_address': request.build_absolute_uri(fs2.url(hashed_file))})
